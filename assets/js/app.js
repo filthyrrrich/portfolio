f3d160b2cb9b50d283623c1d2c465cc2a76cbfb4
Rich Schiffer <richschiffer@Richs-MacBook-Pro.local>
2019-01-02 19:28:29 -0700

    parallax setup

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+// Parallax
+window.addEventListener('scroll', () => {
+    let parent = document.getElementById('parallax-container');
+    let children = parent.getElementsByTagName('div');
+    for(let i = 0; i < children.length; i++) {
+      children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.length) + 'px)';
+    }
+    
+ }, false)
+
+
+ // Swiper for projects
+ var swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 70,
+    stretch: 0,
+    depth: 200,
+    modifier: 2,
+    slideShadows : true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
