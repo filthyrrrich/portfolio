b03736aef29249971b9d20591ad605f9ee71fddc
Rich Schiffer <filthyrrrich@gmail.com>
2019-01-05 19:46:34 -0700

    layout complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,34 @@
-// Parallax
+const nav = document.getElementsByClassName("navbar");
+const links = document.querySelectorAll('.links');
+const sections = document.querySelectorAll('.section');
+
 window.addEventListener('scroll', () => {
+
+    // Parallax
     let parent = document.getElementById('parallax-container');
     let children = parent.getElementsByTagName('div');
     for(let i = 0; i < children.length; i++) {
       children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.length) + 'px)';
     }
-    changeLinkState();
+    
+
+    // Active Links
+    if(sections[2].getBoundingClientRect().top < 100) {
+        changeLinkState(3);
+    } else if(sections[1].getBoundingClientRect().top < 100) {
+        changeLinkState(2);
+    } else if(sections[0].getBoundingClientRect().top < 100) {
+        changeLinkState(1);
+    } else {
+        links.forEach((link) => link.classList.remove('active'));
+    }
+
+    // Navbar Appear
+    if (window.pageYOffset > window.innerHeight / 1.2){
+        nav[0].classList.add("appear");
+    } else {
+        nav[0].classList.remove("appear");
+    }
 
  }, false)
 
@@ -28,22 +51,35 @@ window.addEventListener('scroll', () => {
   },
 });
 
-// Navbar scroll
-const nav = document.getElementsByClassName("navbar");
-window.onscroll = function sticky() {
-    if (window.pageYOffset > window.innerHeight / 1.2){
-        nav[0].classList.add("appear");
-    } else {
-        nav[0].classList.remove("appear");
-    }
+function changeLinkState(x) {
+    links.forEach((link) => link.classList.remove('active'));
+    links[x].classList.add('active');
 }
 
-// applys active on links
-const links = document.querySelectorAll('.links');
-const sections = document.querySelectorAll('.section');
-function changeLinkState() {
-  let index = sections.length;
-  while(--index && window.scrollY + 100 < sections[index].offsetTop);
-  links.forEach((link) => link.classList.remove('active'));
-  links[index].classList.add('active');
+let anchorlinks = document.querySelectorAll('a[href^="#"]')
+ 
+for (let item of anchorlinks) { // relitere 
+    item.addEventListener('click', (e)=> {
+        let hashval = item.getAttribute('href')
+        let target = document.querySelector(hashval)
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+        history.pushState(null, null, hashval)
+        e.preventDefault()
+    })
 }
+// Navbar scroll
+// const nav = document.getElementsByClassName("navbar");
+// window.onscroll = function sticky() {
+//     if (window.pageYOffset > window.innerHeight / 1.2){
+//         nav[0].classList.add("appear");
+//     } else {
+//         nav[0].classList.remove("appear");
+//     }
+// }
+
+// applys active on links
+// const links = document.querySelectorAll('.links');
+// const sections = document.querySelectorAll('.section');
