acd5f222e811b38aad9302e6d7480fa8098a3a46
Rich Schiffer <filthyrrrich@gmail.com>
2019-01-06 19:07:21 -0700

    navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -62,6 +62,25 @@ function changeLinkState(x) {
     links[x].classList.add('active');
 }
 
+const navbarToggler = document.querySelector(".navbar-toggler");
+const navbarMenu = document.querySelector(".navbar ul");
+const navbarLinks = document.querySelectorAll(".navbar a");
+
+navbarToggler.addEventListener("click", navbarTogglerClick);
+
+function navbarTogglerClick() {
+  navbarToggler.classList.toggle("open-navbar-toggler");
+  navbarMenu.classList.toggle("open");
+}
+
+navbarLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
+
+function navbarLinkClick() {
+  if(navbarMenu.classList.contains("open")) {
+    navbarToggler.click();
+  }
+}
+
 // let anchorlinks = document.querySelectorAll('a[href^="#"]')
  
 // for (let item of anchorlinks) { // relitere 
